661cc63e239db7d280e0422ee3049a7d0d425d68
FelipeAlbuquerque900 <felipealbuquerque161@gmail.com>
2025-06-03 18:02:29 -0300

    Primeiro commit da calculadora de IMC

---
+++ imc.py
@@ -0,0 +1,16 @@
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 24.9:
+        return "Peso normal"
+    elif imc < 29.9:
+        return "Sobrepeso"
+    elif imc < 34.9:
+        return "Obesidade grau 1"
+    elif imc < 39.9:
+        return "Obesidade grau 2"
+    else:
+        return "Obesidade grau 3"
